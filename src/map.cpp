a9905c37e39d68b30764982adaee256d3ef203e9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-06-11 18:25:32 +0200

    parse tmx fully

--- src/map.cpp
+++ src/map.cpp
@@ -3,13 +3,13 @@
 #include <iostream>
 #include <iomanip>
 #include "third_party_lib/tinyxml2.h"
+#include "string.h"
 
+using namespace tinyxml2;
 
-static MapLayer parse_layer_from_csv_matrix(std::string csv_matrix);
-static MapLine parse_line_from_csv_line(std::string csv_line);
+static std::vector<MapLayer> parse_layers_from_list(XMLElement* head);
 
 Map Map::from_tmx(const std::string tmx_path) {
-	using namespace tinyxml2;
 
 	XMLDocument map_xml;
 	map_xml.LoadFile(tmx_path.c_str());
@@ -21,10 +21,65 @@ Map Map::from_tmx(const std::string tmx_path) {
   PixelDimension map_src_tileshape = {atoi(root->Attribute("tilewidth")),
                                        atoi(root->Attribute("tileheight"))};
 
-
-	std::vector<MapLayer> layers;
+	// Extract map layers
+	std::vector<MapLayer> layers = parse_layers_from_list(
+			root->FirstChildElement("layer"));
 
 	return Map(layers, map_shape, map_src_tileshape);
+}
+
+static MapLayer parse_layer_from_csv_sheet(const char* csv_sheet);
+static MapLine parse_line_from_csv_line(const char* csv_line);
+
+static std::vector<MapLayer> parse_layers_from_list(XMLElement* head) {
+	std::vector<MapLayer> layers;
+
+	while (head != NULL) {
+		const char* current_layer_csv = head->FirstChildElement("data")->GetText();
+    MapLayer current_layer = parse_layer_from_csv_sheet(current_layer_csv);
+		layers.push_back(current_layer);
+
+		head = head->NextSiblingElement();
+	}
+
+	return layers;
+}
+
+static MapLayer parse_layer_from_csv_sheet(const char* csv_sheet) {
+  MapLayer output;
+
+  const char* line_delim = "\n";
+
+  char* str_line;
+  char* mem;
+  str_line = strtok_r((char*)csv_sheet, line_delim, &mem);
+
+  while (str_line != NULL) {
+    MapLine map_line = parse_line_from_csv_line(str_line);
+    output.push_back(map_line);
+
+    str_line = strtok_r(NULL, line_delim, &mem);
+  }
+
+  return output;
+}
+
+static MapLine parse_line_from_csv_line(const char* csv_line) {
+  MapLine output;
+
+  const char* cell_delim = ",";
+
+  char* cell;
+  char* mem;
+  cell = strtok_r((char*)csv_line, cell_delim, &mem);
+
+  while (cell != NULL) {
+    output.push_back(atoi(cell));
+
+    cell = strtok_r(NULL, cell_delim, &mem);
+  }
+
+  return output;
 }
 
 static void print_map_layer(MapLayer layer);
@@ -46,15 +101,15 @@ void Map::log() {
 }
 
 static void print_map_layer(MapLayer layer) {
-	for (size_t line_idx = 0; layer.size(); line_idx++) {
-		print_map_line(layer.at(line_idx));
+	for (auto & line : layer) {
+		print_map_line(line);
 	}
 	std::cout << std::endl;
 }
 
 static void print_map_line(MapLine line) {
-	for (size_t cell_idx = 0; cell_idx < line.size(); cell_idx++) {
-		std::cout << std::setw(3) << line.at(cell_idx) << " ";
+	for (auto & cell : line) {
+		std::cout << std::setw(3) << cell << " ";
 	}
 	std::cout << "\n";
 }
